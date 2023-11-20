290793b49cb9f344fd9feee7fed87963c350bc74
Andrew Howden <hello@andrewhowden.com>
2023-11-20 15:06:25 +0100

    Artificially limit the gomaxprocs
    This commit artificially limits the gomaxprocs to 1. This is a kind of
    absurd, but reliable way of limiting the performance of the application.
    
    The performance difference is easily reproducable with:
    
      $ siege localhost --reps 10000
    
    With the elapsed time being around ~3 seconds shorter for the
    non-concurrent version.

--- cmd/root.go
+++ cmd/root.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/andrewhowdencom/talks.meshcon.23.pito/server"
 	"github.com/spf13/cobra"
@@ -34,6 +35,7 @@ func Execute() {
 func init() {
 	// Define the path for the configuration file.
 	rootCmd.PersistentFlags().String("config", "/etc/.talks.meshcon.23.pito.yaml", "config file (default is /etc/.talks.meshcon.23.pito.yaml)")
+	rootCmd.PersistentFlags().Int("go-max-procs", 1, "How many processes to assign the Go runtime")
 
 	// Here, we want to demonstrate how difficult it is to reason through the program absent any sort of logging.
 	// Given this, we need it to "swallow" errors in the UI.
@@ -42,9 +44,18 @@ func init() {
 
 func doPreRun(cmd *cobra.Command, args []string) {
 	viper.SetConfigFile(cmd.Flags().Lookup("config").Value.String())
+
+	if err := viper.BindPFlag("go.max-procs", cmd.Flags().Lookup("go-max-procs")); err != nil {
+		// Wow it'd be good if we did something here.
+	}
+
 	if err := viper.ReadInConfig(); err != nil {
 		os.Exit(1)
 	}
+
+	// Set runtime constraints
+	runtime.GOMAXPROCS(viper.GetInt("go.max-procs"))
+
 }
 
 // doRoot is not yet implemented
